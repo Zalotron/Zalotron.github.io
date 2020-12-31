936b082a9a263f6fb48284743017b4e81bd6affa
Zalo <gonzalo.loisotto@gmail.com>
2020-12-31 04:00:59 -0300

    para maggie

--- js/home/main.js
+++ js/home/main.js
@@ -1,23 +1,20 @@
 function onEnter(event, element) {
     gsap.to(element, 0.5, {
-        textShadow: "0px 0px 5px #ff0000, 0px 0px 5px #ff0000 ,0px 0px 5px #ff0000, 0px 0px 5px #ff0000, 0px 0px 5px #ff0000"
+        textShadow: "0px 0px 2px #ffffff, 0px 0px 10px #ff0000 ,0px 0px 20px #ff0000, 0px 0px 50px #ff0000, 0px 0px 100px #ff0000",
+        opacity: 1
     })
 }
 
 function onLeave(event, element) {
     gsap.to(element, 0.5, {
-        textShadow: "0px 0px 0px #000000, 0px 0px 0px #000000, 0px 0px 0px #000000, 0px 0px 0px #000000, 0px 0px 0px #000000"
+        textShadow: "0px 0px 0px #000000, 0px 0px 0px #000000, 0px 0px 0px #000000, 0px 0px 0px #000000, 0px 0px 0px #000000",
+        opacity: 0.5
     })
 }
-
-//function onClick(event, element){
-//
-//}
 
 // SOCIAL BUTTONS
 let socialButtons = document.getElementById("social").children
 Array.from(socialButtons).forEach(element => {
     element.addEventListener("mouseenter", (event) => onEnter(event, element))
     element.addEventListener("mouseleave", (event) => onLeave(event, element))
-    //element.addEventListener("click", (event) => onClick(event, element))
 });
